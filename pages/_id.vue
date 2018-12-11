492baadee047fa26cdf3b58e8b4474c7760de28e
Alexander Lichter <manniL@gmx.net>
2018-12-11 13:36:17 +0000

    Try to repro issue

---
+++ pages/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container">
+    <h1>Origin: {{ origin }}</h1>
+    <br />
+    <pre>
+    {{params}}
+    {{$route.params}}
+    "{{typeof $route.params.id}}" should be equal to "{{typeof params.id}}"
+    </pre>
+  </section>
+</template>
+
+<script>
+export default {
+  asyncData({ params }) {
+    return {
+      origin: process.server ? "server" : "client",
+      params
+    };
+  }
+};
+</script>
